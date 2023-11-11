26962f7386f7a61d625b7cab68c512fd380a954c
Alex Athanasopoulos <alex@melato.org>
2023-11-11 21:15:18 +0200

    TraceFuncs() changes

--- trace.go
+++ trace.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 )
 
-func traceFunc(v *bool) func(bool) { return func(b bool) { *v = b } }
-
-func TraceVariables() map[string]*bool {
-	fmt.Printf("TraceVariables is deprecated.  Use TraceFuncs")
+func traceVariables() map[string]*bool {
 	return map[string]*bool{
 		"data":  &TraceData,
 		"calls": &TraceCalls,
@@ -15,11 +12,16 @@ func TraceVariables() map[string]*bool {
 	}
 }
 
+func TraceVariables() map[string]*bool {
+	fmt.Printf("TraceVariables is deprecated.  Use TraceFuncs\n")
+	return traceVariables()
+}
+
 func traceFunc(v *bool) func(bool) { return func(b bool) { *v = b } }
 
 func TraceFuncs() map[string]func(bool) {
 	funcs := make(map[string]func(bool))
-	for name, v := range TraceVariables() {
+	for name, v := range traceVariables() {
 		funcs[name] = traceFunc(v)
 	}
 	return funcs
