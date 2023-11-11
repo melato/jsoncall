a348c7dd578c5de0f7e83e197745475916a156a0
Alex Athanasopoulos <alex@melato.org>
2023-11-11 20:43:20 +0200

    added TraceFuncs()

--- trace.go
+++ trace.go
@@ -1,11 +1,27 @@
 package jsoncall
 
-// TraceVariables provides the names and references to bool variables
-// that control debugging output.
+import (
+	"fmt"
+)
+
+func traceFunc(v *bool) func(bool) { return func(b bool) { *v = b } }
+
 func TraceVariables() map[string]*bool {
+	fmt.Printf("TraceVariables is deprecated.  Use TraceFuncs")
 	return map[string]*bool{
 		"data":  &TraceData,
 		"calls": &TraceCalls,
 		"init":  &TraceInit,
 	}
+}
+
+func traceFunc(v *bool) func(bool) { return func(b bool) { *v = b } }
+
+func TraceFuncs() map[string]func(bool) {
+	funcs := make(map[string]func(bool))
+	for name, v := range TraceVariables() {
+		funcs[name] = traceFunc(v)
+	}
+	return funcs
+
 }
